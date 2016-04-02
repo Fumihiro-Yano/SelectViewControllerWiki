48684ee85ac088900ee5a79be8fc6b635c35f6f0
矢野史洋 <a2.mj.pop@gmail.com>
2016-04-02 17:27:35 +0900

    ストーリーボード削除

---
+++ SelectViewControllerWiki/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SelectViewControllerWiki
+//
+//  Created by 矢野史洋 on 2016/04/02.
+//  Copyright © 2016年 矢野史洋. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.redColor()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
